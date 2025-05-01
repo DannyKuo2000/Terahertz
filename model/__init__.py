0103784a88472d9346041993872fedc901604561
DannyKuo2000 <dannykuo2000@gmail.com>
2025-05-01 22:13:39 +0800

    verification

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
 from .diffusion250413 import DiffractiveLayer
-from .diffusion250413 import Net
+from .diffusion250413 import ONN
 from .diffusion250413 import Sensor
 from .diffusion250413 import ConditionedUNet
 from .diffusion250413 import DiffusionDecoder
